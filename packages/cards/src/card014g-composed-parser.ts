5876bf74893aec6dac8fcf6abd67a3db6ef84741
cwmle <cwmlee00@gmail.com>
2026-05-24 08:59:05 -0500

    Remove cards package

--- packages/cards/src/card014g-composed-parser.ts
+++
@@ -1,7 +0,0 @@
-export {
-  isOnPlayFieldEffectModifierWording,
-  parseCard014gClause,
-  parseCard014gResidueClause,
-  type Card014gCertifiedClause,
-  type Card014gResidueClause,
-} from "./on-play-field-effect-parser.js";
