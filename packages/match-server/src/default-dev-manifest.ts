c8e7469a84754e4c5c895b1ffec40a6d51176642
cwmle <cwmlee00@gmail.com>
2026-06-10 19:49:15 -0500

    Bump dev effect definition cache version

--- packages/match-server/src/default-dev-manifest.ts
+++ packages/match-server/src/default-dev-manifest.ts
@@ -144,7 +144,7 @@ export const defaultDevDonCounts: DevDonCounts = {
   secondPlayer: 10,
 };
 
-export const defaultDevEffectDefinitionsVersion = "generated-dev-v9";
+export const defaultDevEffectDefinitionsVersion = "generated-dev-v10";
 const defaultDevDeckValidatorVersion = "dev-deck-validator-v3";
 export const defaultDevDeckFormatId = "sandbox-open";
 const defaultDevCatalogVersionsTtlMs = 60_000;
