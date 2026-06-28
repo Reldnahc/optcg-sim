c88db070902fac90847bf730dc927ea15cbb2ea9
cwmle <cwmlee00@gmail.com>
2026-06-28 18:26:17 -0500

    Revert "Make deck selector crop honor focus"
    This reverts commit d8959d1f48c1a68f9f749d199b1c3fb95377b661.

--- packages/client/src/react/CanonicalCardCropImage.test.ts
+++ packages/client/src/react/CanonicalCardCropImage.test.ts
@@ -4,7 +4,6 @@ import { describe, test } from "vitest";
 import {
   CARD_CROP_LEGAL_RECT,
   resolveCanonicalCropCenter,
-  resolveFocalCropImageLayout,
 } from "./CanonicalCardCropImage.js";
 
 describe("CanonicalCardCropImage", () => {
@@ -28,21 +27,5 @@ describe("CanonicalCardCropImage", () => {
     assert.equal(crop.x.toFixed(6), "0.501500");
     assert.equal(crop.y.toFixed(6), "0.203183");
     assert.equal(crop.zoom.toFixed(6), "1.808318");
-  });
-
-  test("uses focal points to position deck selector art", () => {
-    const upperFocus = resolveFocalCropImageLayout({
-      cropFocus: { x: 0.42, y: 0.18 },
-      frameSize: { width: 112, height: 68 },
-      imageSize: { width: 630, height: 880 },
-    });
-    const lowerFocus = resolveFocalCropImageLayout({
-      cropFocus: { x: 0.7, y: 0.6 },
-      frameSize: { width: 112, height: 68 },
-      imageSize: { width: 630, height: 880 },
-    });
-
-    assert.notEqual(upperFocus.left.toFixed(2), lowerFocus.left.toFixed(2));
-    assert.notEqual(upperFocus.top.toFixed(2), lowerFocus.top.toFixed(2));
   });
 });
